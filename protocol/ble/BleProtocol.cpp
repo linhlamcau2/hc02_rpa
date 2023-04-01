36da1e85b0bae66a2b7c868eece70b5b5c42aa75
thinpv <thindcna@gmail.com>
2023-04-01 16:13:52 +0700

    fix build err

--- protocol/ble/BleProtocol.cpp
+++ protocol/ble/BleProtocol.cpp
@@ -458,7 +458,7 @@ int BleProtocol::AddDevice(scan_device_message_t *scan_device_message)
 							deviceType = convertDeviceType(deviceType);
 							Json::Value devKeyJson;
 							devKeyJson["devicekey"] = Util::arrayToString844412((uint8_t *)deviceKey);
-							Device *device = gateway->AddNewDevice(Util::uuidToStr(uuid), Device::ConvertDeviceTypeToName(deviceType), mac, devKeyJson.toString(), nextAddr, deviceType, version, true, true);
+							Device *device = gateway->AddNewDevice(Util::uuidToStr(uuid->uuid), Device::ConvertDeviceTypeToName(deviceType), mac, devKeyJson.toString(), nextAddr, deviceType, version, true, true);
 							if (device)
 							{
 								gateway->AddDeviceToScanList(device);
