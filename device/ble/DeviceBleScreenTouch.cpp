8726a1737895d39f4bedc8fc895c9530d04d77f6
thindcna <thindcna@gmail.com>
2023-04-26 14:43:51 +0700

    fix create task esp32

--- device/ble/DeviceBleScreenTouch.cpp
+++ device/ble/DeviceBleScreenTouch.cpp
@@ -4,24 +4,34 @@
 #include "Util.h"
 #include "Log.h"
 
+static void SendDatetime(void *data);
+
 DeviceBleScreenTouch::DeviceBleScreenTouch(string id, string name, string mac, string data, uint32_t addr, uint16_t version)
 		: DeviceBle(id, name, mac, data, addr, BLE_AC_SCENE_SCREEN_TOUCH, version)
 {
 	moduleNotifyScene = new ModuleNotifyScene(this, addr);
 	modules.push_back(moduleNotifyScene);
-	thread sendDateTimeThread(bind(&DeviceBleScreenTouch::SendDatetime, this));
-	sendDateTimeThread.detach();
 	powerSource = POWER_AC;
+
+#ifdef ESP_PLATFORM
+	xTaskCreate(SendDatetime, "SendDatetime", 2048, this, 10, NULL);
+	vTaskDelay(10);
+#else
+	thread SendDatetimeThread(SendDatetime, this);
+	SendDatetimeThread.detach();
+#endif
 }
 
-void DeviceBleScreenTouch::SendDatetime()
+static void SendDatetime(void *data)
 {
+	LOGD("SendDatetime Start");
+	DeviceBleScreenTouch *deviceBleScreenTouch = (DeviceBleScreenTouch *)data;
 	while (1)
 	{
 		if (bleProtocol)
 		{
-			bleProtocol->SendDate(addr, Util::GetYearsCurrent(), Util::GetMonthsCurrent(), Util::GetDateCurrent(), Util::GetDaysCurrent());
-			bleProtocol->SendTime(addr, Util::GetHoursCurrent(), Util::GetMinutesCurrent(), Util::GetSecondsCurrent());
+			bleProtocol->SendDate(deviceBleScreenTouch->GetAddr(), Util::GetYearsCurrent(), Util::GetMonthsCurrent(), Util::GetDateCurrent(), Util::GetDaysCurrent());
+			bleProtocol->SendTime(deviceBleScreenTouch->GetAddr(), Util::GetHoursCurrent(), Util::GetMinutesCurrent(), Util::GetSecondsCurrent());
 		}
 		else
 			LOGW("BleProtocol null");
