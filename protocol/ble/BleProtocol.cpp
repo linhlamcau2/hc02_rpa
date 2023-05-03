8d9add2ac8cce207387118d64543a9e4753edc59
thindcna <thindcna@gmail.com>
2023-05-03 07:45:14 +0700

    fix build

--- protocol/ble/BleProtocol.cpp
+++ protocol/ble/BleProtocol.cpp
@@ -281,13 +281,13 @@ int BleProtocol::SendMessage(uint16_t opReq, uint8_t *dataReq, int lenReq, uint8
 	if (pthread_mutex_lock(&mutex) == 0)
 	{
 		message_rsp_list_st message_rsp_list = {
-			.status = false,
-			.opcode = opRsp,
-			.len = lenRsp,
-			.data = dataRsp,
-			.compare_data = compare_data,
-			.compare_position = compare_position,
-			.compare_len = compare_len,
+				.status = false,
+				.opcode = opRsp,
+				.len = lenRsp,
+				.data = dataRsp,
+				.compare_data = compare_data,
+				.compare_position = compare_position,
+				.compare_len = compare_len,
 		};
 		if (opRsp)
 		{
@@ -296,7 +296,7 @@ int BleProtocol::SendMessage(uint16_t opReq, uint8_t *dataReq, int lenReq, uint8
 		}
 
 		message_req_st message_req = {
-			.opcode = opReq,
+				.opcode = opReq,
 		};
 		for (int i = 0; i < lenReq; i++)
 		{
@@ -505,8 +505,8 @@ int BleProtocol::ResetBle()
 	if (rs)
 	{
 		LOGE("Send reset factory error, rs: %d", rs);
-		return rs;
 	}
+	return rs;
 }
 
 int BleProtocol::ResetFactory()
@@ -514,7 +514,7 @@ int BleProtocol::ResetFactory()
 	LOGD("ResetFactory");
 	InitKey();
 	database->GatewayRead();
-	return rs;
+	return CODE_OK;
 }
 
 static uint32_t convertDeviceType(uint32_t type)
@@ -914,9 +914,9 @@ int BleProtocol::SetOnOffLight(uint16_t devAddr, uint8_t onoff, uint16_t transit
 			uint16_t gwAddr;
 			uint16_t opcodeRsp;
 		} turnOnOffHeader = {
-			.devAddr = devAddr,
-			.gwAddr = 0x0001,
-			.opcodeRsp = G_ONOFF_STATUS,
+				.devAddr = devAddr,
+				.gwAddr = 0x0001,
+				.opcodeRsp = G_ONOFF_STATUS,
 		};
 		onoff_message.ble_message_header.devAddr = devAddr;
 		onoff_message.opcode = G_ONOFF_SET;
@@ -1838,9 +1838,9 @@ int BleProtocol::SetScenePirLightSensor(uint16_t devAddr, uint8_t condition, uin
 			uint32_t data;
 			struct
 			{
-				uint32_t store : 8;			 // 8 bit not use
-				uint32_t Lux_hi : 10;		 // 10 bit lux hi
-				uint32_t Lux_low : 10;		 // 10 bit lux low
+				uint32_t store : 8;					 // 8 bit not use
+				uint32_t Lux_hi : 10;				 // 10 bit lux hi
+				uint32_t Lux_low : 10;			 // 10 bit lux low
 				uint32_t Light_Conditon : 3; // 7 bit low
 				uint32_t Pir_Conditon : 1;	 // 1 bit hight
 			};
@@ -2966,8 +2966,8 @@ int BleProtocol::UpdateDeviceKeyDev(uint16_t devAddr, string devKeyDev)
 			uint8_t devKey[16];
 		} update_devkey_device_t;
 		update_devkey_device_t update_devkey_device = {
-			.header = 0x12,
-			.devAddr = devAddr};
+				.header = 0x12,
+				.devAddr = devAddr};
 		update_devkey_device.element = 0x0002;
 		for (int i = 0; i < 16; i++)
 		{
@@ -2995,8 +2995,8 @@ int BleProtocol::UpdateDeviceKeyGateway(uint16_t gwAddr, string devKeyDev)
 			uint8_t devKey[16];
 		} update_devkey_device_t;
 		update_devkey_device_t update_devkey_device = {
-			.header = 0x12,
-			.devAddr = gwAddr};
+				.header = 0x12,
+				.devAddr = gwAddr};
 		update_devkey_device.element = 0x0001;
 		for (int i = 0; i < 16; i++)
 		{
