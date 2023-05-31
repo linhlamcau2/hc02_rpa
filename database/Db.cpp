67b5735997bd3398c6160fc3dd37f3886cc29569
vuhongtu1997 <tuvuvu2121@gmail.com>
2023-05-31 14:44:22 +0700

    add db device/scene favorite

--- database/Db.cpp
+++ database/Db.cpp
@@ -96,7 +96,7 @@ bool Db::IsHaveDb()
 int Db::createTableIfNotExists()
 {
 #ifdef ESP_PLATFORM
-	string sql = "CREATE TABLE IF NOT EXISTS Device (mac VARCHAR, device_id VARCHAR NOT NULL, name VARCHAR, addr INTEGER, type INTEGER, firmware_version VARCHAR, hardware_version VARCHAR, active_time INTEGER, update_time INTEGER, data TEXT, PRIMARY KEY (device_id)) WITHOUT ROWID;"
+	string sql = "CREATE TABLE IF NOT EXISTS Device (mac VARCHAR, device_id VARCHAR NOT NULL, name VARCHAR, addr INTEGER, type INTEGER, firmware_version VARCHAR, hardware_version VARCHAR, active_time INTEGER, update_time INTEGER, data TEXT, is_favorite BLOB, PRIMARY KEY (device_id)) WITHOUT ROWID;"
 				 "CREATE TABLE IF NOT EXISTS DeviceAttribute (device_id VARCHAR NOT NULL, attribute_id INTEGER, value DOUBLE, PRIMARY KEY (device_id, attribute_id)) WITHOUT ROWID;"
 				 "CREATE TABLE IF NOT EXISTS DeviceBleChild (device_id VARCHAR NOT NULL, element INTEGER NOT NULL, PRIMARY KEY (device_id, element)) WITHOUT ROWID;"
 				 "CREATE TABLE IF NOT EXISTS DeviceInGroup (group_id VARCHAR NOT NULL, device_id VARCHAR NOT NULL, element INTEGER, PRIMARY KEY (group_id, device_id, element)) WITHOUT ROWID;"
@@ -109,7 +109,7 @@ int Db::createTableIfNotExists()
 				 "CREATE TABLE IF NOT EXISTS SceneBle (scene_ble_id VARCHAR NOT NULL, scene_ble_addr INTEGER, name VARCHAR, room_id TEXT, PRIMARY KEY (scene_ble_id)) WITHOUT ROWID;"
 				 "CREATE TABLE IF NOT EXISTS SceneDelay (scene_delay_id VARCHAR NOT NULL, scene_delay_addr INTEGER, name VARCHAR, data TEXT, PRIMARY KEY (scene_delay_id)) WITHOUT ROWID;";
 #else
-	string sql = "CREATE TABLE IF NOT EXISTS Device (mac VARCHAR, device_id VARCHAR NOT NULL, name VARCHAR, addr INTEGER, type INTEGER, firmware_version VARCHAR, hardware_version VARCHAR, active_time INTEGER, update_time INTEGER, data TEXT, PRIMARY KEY (device_id));"
+	string sql = "CREATE TABLE IF NOT EXISTS Device (mac VARCHAR, device_id VARCHAR NOT NULL, name VARCHAR, addr INTEGER, type INTEGER, firmware_version VARCHAR, hardware_version VARCHAR, active_time INTEGER, update_time INTEGER, data TEXT, is_favorite BLOB, PRIMARY KEY (device_id));"
 				 "CREATE TABLE IF NOT EXISTS DeviceAttribute (device_id VARCHAR NOT NULL, attribute_id INTEGER, value DOUBLE, PRIMARY KEY (device_id, attribute_id));"
 				 "CREATE TABLE IF NOT EXISTS DeviceBleChild (device_id VARCHAR NOT NULL, element INTEGER NOT NULL, PRIMARY KEY (device_id, element));"
 				 "CREATE TABLE IF NOT EXISTS DeviceInGroup (group_id VARCHAR NOT NULL, device_id VARCHAR NOT NULL, element INTEGER, PRIMARY KEY (group_id, device_id, element));"
@@ -119,7 +119,7 @@ int Db::createTableIfNotExists()
 				 "CREATE TABLE IF NOT EXISTS [Group] (group_id VARCHAR NOT NULL, group_addr INTEGER, name VARCHAR, room_id TEXT, PRIMARY KEY (group_id));"
 				 "CREATE TABLE IF NOT EXISTS Room (room_id VARCHAR NOT NULL, room_addr INTEGER, name VARCHAR, data TEXT, PRIMARY KEY (room_id));"
 				 "CREATE TABLE IF NOT EXISTS Rule (rule_id VARCHAR NOT NULL, data TEXT NOT NULL, type INTEGER, enable BOOLEAN, rule_addr INTEGER, PRIMARY KEY (rule_id));"
-				 "CREATE TABLE IF NOT EXISTS SceneBle (scene_ble_id VARCHAR NOT NULL, scene_ble_addr INTEGER, name VARCHAR, room_id TEXT, PRIMARY KEY (scene_ble_id));"
+				 "CREATE TABLE IF NOT EXISTS SceneBle (scene_ble_id VARCHAR NOT NULL, scene_ble_addr INTEGER, name VARCHAR, room_id TEXT, is_favorite BLOB, PRIMARY KEY (scene_ble_id));"
 				 "CREATE TABLE IF NOT EXISTS SceneDelay (scene_delay_id VARCHAR NOT NULL, scene_delay_addr INTEGER, name VARCHAR, data TEXT, PRIMARY KEY (scene_delay_id));";
 #endif
 	return Sqlite_Exec(sql);
