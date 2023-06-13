33c68f31ff22159c4e4723399cc19eb45d8429ef
thindcna <thindcna@gmail.com>
2023-06-13 18:34:50 +0700

    remove TODO

--- device/mqtt/DeviceMqttAihub.h
+++ device/mqtt/DeviceMqttAihub.h
@@ -1,13 +1,11 @@
 #pragma once
 
 #include "DeviceMqtt.h"
-#include "function/FunctionZone.h"
 
 using namespace std;
 
 class DeviceMqttAihub : public DeviceMqtt
 {
-private:
 public:
 	DeviceMqttAihub(string id, string name, string mac, string data, uint16_t version);
 };
