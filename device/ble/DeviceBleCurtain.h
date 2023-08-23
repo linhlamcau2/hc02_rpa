4fec5de695ae7becf7f35d2e7f41b0e1880465bf
nguyenvietanh15081997 <nguyenvietanh15081997@gmail.com>
2023-08-23 14:51:32 +0700

    fix device curtain

--- device/ble/DeviceBleCurtain.h
+++ device/ble/DeviceBleCurtain.h
@@ -3,7 +3,7 @@
 #include "DeviceBle.h"
 #include "module/ModuleCurtain.h"
 #include "module/ModuleRgb.h"
-#include "module/ModuleSelectMotor.h"
+// #include "module/ModuleSelectMotor.h"
 
 using namespace std;
 
@@ -12,7 +12,7 @@ class DeviceBleCurtain : public DeviceBle
 private:
     ModuleCurtain *moduleCurtain;
     ModuleRgb *moduleRgb;
-    ModuleSelectMotor *moduleSelectMotor;
+    // ModuleSelectMotor *moduleSelectMotor;
 
 public:
 	DeviceBleCurtain(string id, string name, string mac, Json::Value &dataJson, uint16_t addr, uint32_t type, uint16_t version);
