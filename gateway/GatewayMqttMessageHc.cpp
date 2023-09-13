0482947d59b2dc14e45dd9a9c20b5b3f1ac61623
thinpv <thindcna@gmail.com>
2023-09-13 07:55:57 +0700

    add create tunnel

--- gateway/GatewayMqttMessageHc.cpp
+++ gateway/GatewayMqttMessageHc.cpp
@@ -13,7 +13,8 @@ void Gateway::InitMqttMessageHc()
 	OnDeviceRpcCallbackRegister("stopScanBle", bind(&Gateway::OnStopScanBle, this, placeholders::_1, placeholders::_2));
 	OnDeviceRpcCallbackRegister("resetHc", bind(&Gateway::OnResetHC, this, placeholders::_1, placeholders::_2));
 	OnDeviceRpcCallbackRegister("versionHc", bind(&Gateway::OnVersionHC, this, placeholders::_1, placeholders::_2));
-	OnDeviceRpcCallbackRegister("SSHRemote", bind(&Gateway::OnSSHRemote, this, placeholders::_1, placeholders::_2));
+	OnDeviceRpcCallbackRegister("CreateTunnel", bind(&Gateway::OnCreateTunnel, this, placeholders::_1, placeholders::_2));
+	OnDeviceRpcCallbackRegister("DeleteAllTunnel", bind(&Gateway::OnDeleteAllTunnel, this, placeholders::_1, placeholders::_2));
 
 	OnLocalCallbackRegister("controlHc", bind(&Gateway::OnControlHc, this, placeholders::_1, placeholders::_2));
 	OnLocalCallbackRegister("getHcInfo", bind(&Gateway::OnGetHcInfo, this, placeholders::_1, placeholders::_2));
@@ -21,7 +22,6 @@ void Gateway::InitMqttMessageHc()
 	OnLocalCallbackRegister("stopScanBle", bind(&Gateway::OnStopScanBle, this, placeholders::_1, placeholders::_2));
 	OnLocalCallbackRegister("resetHc", bind(&Gateway::OnResetHC, this, placeholders::_1, placeholders::_2));
 	OnLocalCallbackRegister("versionHc", bind(&Gateway::OnVersionHC, this, placeholders::_1, placeholders::_2));
-	OnLocalCallbackRegister("SSHRemote", bind(&Gateway::OnSSHRemote, this, placeholders::_1, placeholders::_2));
 }
 
 int Gateway::OnControlHc(Json::Value &reqValue, Json::Value &respValue)
@@ -126,80 +126,111 @@ int Gateway::OnVersionHC(Json::Value &reqValue, Json::Value &respValue)
 	return CODE_OK;
 }
 
-int Gateway::OnSSHRemote(Json::Value &reqValue, Json::Value &respValue)
+int Gateway::OnCreateTunnel(Json::Value &reqValue, Json::Value &respValue)
 {
-	int rs = CODE_OK;
-	if (reqValue.isMember("type") && reqValue["type"].isString() &&
-			reqValue.isMember("key") && reqValue["key"].isString() &&
-			reqValue.isMember("user") && reqValue["user"].isString() &&
-			reqValue.isMember("host") && reqValue["host"].isString() &&
-			reqValue.isMember("serverPort") && reqValue["serverPort"].isInt() &&
-			reqValue.isMember("forwardPort") && reqValue["forwardPort"].isInt())
+	int err = 0;
+	LOGD("Create Tunnel");
+	if (reqValue.isMember("params") && reqValue["params"].isObject())
 	{
-		string key = "";
-		string type = reqValue["type"].asString();
-		string user = reqValue["user"].asString();
-		string host = reqValue["host"].asString();
-		uint32_t serverPort = reqValue["serverPort"].asInt();
-		uint32_t forwardPort = reqValue["forwardPort"].asInt();
-		uint32_t localPort = 22;
-		if (reqValue.isMember("localPort") && reqValue["localPort"].isInt())
+		Json::Value params = reqValue["params"];
+		if (params.isMember("type") && params["type"].isString() &&
+				params.isMember("key") && params["key"].isString() &&
+				params.isMember("user") && params["user"].isString() &&
+				params.isMember("host") && params["host"].isString() &&
+				params.isMember("serverPort") && params["serverPort"].isInt() &&
+				params.isMember("forwardPort") && params["forwardPort"].isInt())
 		{
-			localPort = reqValue["localPort"].asInt();
-		}
-		if (type == "base64")
-		{
-			string keyBase64 = reqValue["key"].asString();
-			string decode = macaron::Base64::Decode(keyBase64, key);
-			if (decode != "")
+			string key = "";
+			string type = params["type"].asString();
+			string user = params["user"].asString();
+			string host = params["host"].asString();
+			uint32_t serverPort = params["serverPort"].asInt();
+			uint32_t forwardPort = params["forwardPort"].asInt();
+
+			uint32_t localPort = 22;
+			if (params.isMember("localPort") && params["localPort"].isInt())
 			{
-				rs = CODE_ERROR;
-				LOGW("Base64 decode err: %s", decode.c_str());
+				localPort = params["localPort"].asInt();
+			}
+			LOGI("Create Tunnel %d --> %s:%d", localPort, host.c_str(), forwardPort);
+			if (type == "base64")
+			{
+				string keyBase64 = params["key"].asString();
+				string decode = macaron::Base64::Decode(keyBase64, key);
+				if (decode != "")
+				{
+					err = 1;
+					LOGW("Base64 decode err: %s", decode.c_str());
+				}
+			}
+			else
+			{
+				key = params["key"].asString();
+			}
+
+			if (err == 0)
+			{
+				// save key file
+				system("rm " TMP_FOLDER "key.txt");
+				system("rm " TMP_FOLDER "output.txt");
+				ofstream keyFile(TMP_FOLDER "key.txt");
+				keyFile << key;
+				keyFile.close();
+
+				system("chmod 600 " TMP_FOLDER "key.txt");
+				// system("killall ssh");
+				string cmd = "ssh -i " TMP_FOLDER "key.txt -o StrictHostKeyChecking=no -f -N -T -R" + to_string(forwardPort) + ":localhost:" + to_string(localPort) + " " + user + "@" + host + " -p " + to_string(serverPort);
+				cmd += " >> " TMP_FOLDER "output.txt 2>&1";
+				LOGI("cmd: %s", cmd.c_str());
+				system(cmd.c_str());
+				sleep(2);
+				bool err = false;
+				FILE *fp = fopen("" TMP_FOLDER "output.txt", "r");
+				char path[512] = {0};
+				if (fp)
+				{
+					while (fgets(path, sizeof(path), fp) != NULL)
+					{
+						if (strlen(path) > 1)
+						{
+							LOGW("SSH err: %s", path);
+							err = true;
+							break;
+						}
+					}
+					fclose(fp);
+				}
+				if (err)
+				{
+					respValue["msg"] = string(path);
+					respValue["code"] = 1;
+				}
+				else
+				{
+					respValue["code"] = 0;
+				}
+				return CODE_OK;
 			}
 		}
 		else
 		{
-			key = reqValue["key"].asString();
-		}
-
-		if (rs == 0)
-		{
-			// save key file
-			system("rm /key.txt");
-			system("rm /output.txt");
-			ofstream keyFile("/key.txt");
-			keyFile << key;
-			keyFile.close();
-
-			system("chmod 600 /key.txt");
-			system("killall ssh");
-			string cmd = "ssh -i /key.txt -o StrictHostKeyChecking=no -f -N -T -R" + to_string(forwardPort) + ":localhost:" + to_string(localPort) + " " + user + "@" + host + " -p " + to_string(serverPort);
-			cmd += " >> /output.txt 2>&1";
-			LOGI("cmd: %s", cmd.c_str());
-			system(cmd.c_str());
-			sleep(2);
-			FILE *fp = fopen("/output.txt", "r");
-			char path[512] = {0};
-			if (fp)
-			{
-				while (fgets(path, sizeof(path), fp) != NULL)
-				{
-					if (strlen(path) > 1)
-					{
-						LOGW("SSH err: %s", path);
-						rs = CODE_ERROR;
-						break;
-					}
-				}
-				fclose(fp);
-			}
+			LOGE("Error data");
+			respValue["data"]["code"] = CODE_FORMAT_ERROR;
 		}
 	}
 	else
 	{
-		rs = CODE_FORMAT_ERROR;
+		LOGW("Error");
 	}
-	respValue["code"] = rs;
-	respValue["cmd"] = "SSHRemoteRsp";
+
+	respValue["code"] = err;
+	return CODE_OK;
+}
+
+int Gateway::OnDeleteAllTunnel(Json::Value &reqValue, Json::Value &respValue)
+{
+	LOGD("Delete all Tunnel");
+	system("killall ssh");
+	respValue["code"] = 0;
 	return CODE_OK;
 }
