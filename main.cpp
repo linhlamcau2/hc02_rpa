39b6b55540d9438de0abb3d8990a3e533db9dd25
nguyenvietanh15081997 <nguyenvietanh15081997@gmail.com>
2023-12-04 10:02:57 +0700

    fix local connect info

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 	// string mac = "be:3e:25:2e:a1:ca";
 	LOGI("mac: %s", mac.c_str());
 	gateway = new Gateway(mac, config->GetHost(), config->GetPort(), "hc-" + mac, "hc-" + mac, config->GetPassword(), config->GetKeepAlive(),
-						  config->GetLocalHost(), config->GetLocalPort(), config->GetLocalUsername(), config->GetLocalPassword(), 10);
+						  "localhost", 1883, "RD", "", 10);
 	gateway->init();
 
 	bleProtocol->InitKey();
